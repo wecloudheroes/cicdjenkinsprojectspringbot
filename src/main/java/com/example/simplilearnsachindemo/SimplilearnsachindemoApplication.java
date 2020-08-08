84c6a18defe1612dc4944e4c31d57137b10c0915
sachin devops <sachindevops@outlook.com>
2020-08-08 22:55:43 +0530

    Commit done

---
+++ src/main/java/com/example/simplilearnsachindemo/SimplilearnsachindemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplilearnsachindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnsachindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnsachindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplilearnsachindemo/SimplilearnsachindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplilearnsachindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnsachindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
